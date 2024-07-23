07e000ba7b9ba1df38c2a70c4f9fad8204e0b7b1
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-23 14:49:11 +0700

    create 2 files and move 1 file

---
+++ contents/nghia-view-tree.py
@@ -0,0 +1,37 @@
+
+import os
+
+
+def display(directory, indent='', output_file=None, ignore_folders=None):
+    folder_name = os.path.basename(directory)
+    if folder_name not in ignore_folders:
+        with open(output_file, "a") as file:
+            file.write(indent + "----" + folder_name + "/\n")
+
+    indent += "    |"
+
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            if os.path.basename(item_path) not in ignore_folders:
+                display(item_path, indent, output_file, ignore_folders)
+        else:
+            if folder_name not in ignore_folders:
+                with open(output_file, "a") as file:
+                    file.write(indent + "----" + item + "\n")
+
+
+#! INPUT
+folder_path = r"C:\Users\VuVanNghia20206205\Desktop\flask-hello-world"
+folder_path = r"input"
+folder_path = r"/home/vvn20206205/Desktop/Git/company20206205"
+
+
+output_file = "output.txt"
+ignore_folders = [".git", "node_modules", "__pycache__"]
+#! INPUT
+with open(output_file, 'w') as file:
+    file.write("")
+display(folder_path, output_file=output_file, ignore_folders=ignore_folders)
+print(f"Kết quả đã được ghi vào tập tin {output_file}")
+
