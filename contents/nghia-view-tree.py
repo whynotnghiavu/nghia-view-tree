b779eb1227f9958163d9ca0ae82ca49d323624db
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-21 15:15:15 +0700

    update nghia-view-tree.py and output.txt

--- contents/nghia-view-tree.py
+++ contents/nghia-view-tree.py
@@ -26,12 +26,14 @@ folder_path = r"input"
 folder_path = r"/home/vvn20206205/Desktop/Git/company20206205"
 folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu"
 folder_path = r"C:\Users\vvn20206205\Downloads\video\Fast Typing Mastery From Beginner to Expert in 45 Minutes"
+folder_path = r"C:\Users\vvn20206205\Downloads\video\Fast_Typing_Mastery_From_Beginner_to_Expert_in_45_Minutes_2\Fast Typing Mastery From Beginner to Expert in 45 Minutes"
 
 
 output_file = "output.txt"
 ignore_folders = [".git", "node_modules", "__pycache__"]
 #! INPUT
 with open(output_file, 'w', encoding="utf-8") as file:
-    file.write("")
+    file.write(f"PATH: {folder_path}\n\n\n")
+
 display(folder_path, output_file=output_file, ignore_folders=ignore_folders)
 print(f"Kết quả đã được ghi vào tập tin {output_file}")
